e0ea8e0bb8de8ab4c0a06f204de5cc0fcb97f727
norman <norman.wlf@outlook.com>
2017-08-15 17:44:26 +0800

    change local sqllit db into a remote mongo db

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import pymongo
+
+mongodb_name = 'chatterbot'
+mongodb_uri = 'mongodb://9.111.111.203'
+
 
 app = Flask(__name__)
 
-english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+#english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+english_bot = ChatBot("English Bot", 
+                     storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
+                     database = mongodb_name,
+                     database_uri = mongodb_uri)
 
 english_bot.set_trainer(ChatterBotCorpusTrainer)
 english_bot.train("chatterbot.corpus.english")
